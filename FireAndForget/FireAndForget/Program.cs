f8ba333e4b840bb786ccda351585cf2d58dbe8aa
newmancroos <newmancroos@gmail.com>
2020-09-09 14:53:43 -0400

    Completed Fire and Forget

---
+++ FireAndForget/FireAndForget/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FireAndForget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Task.Run(() => FireAndForgetIt("Newman")).ConfigureAwait(false); // If exception in the calling function will stop the application
+            Task.Run(() => FireAndForgetIt("Newmancroos")).Forget();
+            Console.WriteLine("Hello World111!");
+            Console.ReadLine();
+        }
+
+        public static void FireAndForgetIt(string input)
+        {
+            
+            Thread.Sleep(5000);
+            Console.WriteLine(input);
+            throw new ArgumentException("Error"); 
+        }
+    }
+
+
+}
---
+++ FireAndForget/FireAndForget/TaskExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+
+namespace FireAndForget
+{
+    public static class TaskExtension
+    {
+        public static void Forget(this Task task)
+        {
+            if (!task.IsCompleted || task.IsFaulted)
+            {
+                _ = ForgetAwaited(task);
+            }
+        }
+
+        async static Task ForgetAwaited(Task task)
+        {
+            try
+            {
+                await task.ConfigureAwait(false);
+            }
+            catch
+            {
+
+                //Do nothing
+            }
+        }
+    }
+}
