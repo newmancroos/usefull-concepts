3178a1f41f5acb9e43dc9bc7ccb12ae87c48df31
newmancroos <newmancroos@gmail.com>
2020-09-29 10:58:03 -0400

    Fire and forget testing

--- FireAndForget/FireAndForget/Program.cs
+++ FireAndForget/FireAndForget/Program.cs
@@ -12,6 +12,11 @@ namespace FireAndForget
             //Task.Run(() => FireAndForgetIt("Newman")).ConfigureAwait(false); // If exception in the calling function will stop the application
             Task.Run(() => FireAndForgetIt("Newmancroos")).Forget();
             Console.WriteLine("Hello World111!");
+            //---------------------------
+            DateTime dateTime1 = DateTime.Now.AddMinutes(-10);
+            int MinDiff = ((TimeSpan)(DateTime.Now - dateTime1)).Minutes;
+            Console.WriteLine($"Difference : {MinDiff}");
+            //-----------------------------
             Console.ReadLine();
         }
 
